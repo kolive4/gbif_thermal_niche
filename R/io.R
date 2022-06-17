4631e8cb45769efec47d16f62d8bf7b50af3a75d
Ben Tupper <btupper@bigelow.org>
2022-06-17 08:43:08 -0400

    factored new functionality developed earlier into package

--- R/io.R
+++ R/io.R
@@ -1,24 +1,93 @@
-set_data_path = function(path = rappdirs::user_data_dir("gbif_niche")){
-  if(!dir.exists(path)){
-    okay = dir.create(path, recursive = TRUE)
-  }
-  return(path)
+#' Retrieve a data path
+#' 
+#' @export
+#' @param ... charcater, one or more file path segments to be post-pended to \code{root}
+#' @param root character, the root data directory path
+#' @return charcater path specification
+get_path <- function(..., root = rappdirs::user_data_dir("gbif_niche")){
+  file.path(root, ...)
 }
 
-# maybe make zz doc
+# # fetch occurrence data for given species from GBIF
+# # 
+# # @export
+# # @param species character, latin name of species to fetch
+# # @param cache logical, if true save results to cache
+# # @return data frame in the form of a tibble (or an error object if issues arise)
+# fetch_gbif = function(species = "Carcharodon carcharias", cache = TRUE){
+#   occurrence <- try(rgbif::occ_search(scientificName = species[1]))
+#   if (!inherits(occurrence, "try-error")){
+#     occurrence <- occurrence$data
+#     if(cache == TRUE){
+#       path <- get_path(paste0(species, ".csv.gz"))
+#       occurrence <- readr::write_csv(occurrence, path)
+#     }
+#   }
+#   return(occurrence)
+# }
 
-#' fetch occurrence data for given species from GBIF
+#' Fetch occurrence data for given species from GBIF
 #' 
 #' @export
 #' @param species character, latin name of species to fetch
 #' @param cache logical, if true save results to cache
+#' @param verbose logical, if true prints counter of progress
+#' @param progress logical, if true print progress bar and ignore verbose
 #' @return data frame in the form of a tibble
-fetch_gbif = function(species = "Carcharodon carcharias", cache = TRUE){
-  occurrence <- rgbif::occ_search(scientificName = species[1])$data
+fetch_gbif = function(species = "Carcharodon carcharias", 
+                      cache = TRUE, 
+                      verbose = FALSE, 
+                      progress = !verbose){
+  
+  if(progress == TRUE){
+    verbose = FALSE
+  }
+  
+  DONE = FALSE
+  LIMIT = 500
+  CUR = 0
+  FROM = "gbif"
+  
+  x <- spocc::occ(query = species[1], limit = LIMIT, from = FROM, 
+                  start = CUR, throw_warnings = FALSE)
+  COUNT = x[[FROM]]$meta$found
+  NCHUNKS = ceiling(COUNT / LIMIT)
+  ICHUNK = 1
+  
+  xx = vector(mode = "list", length = NCHUNKS)
+  xx[[ICHUNK]] <- x[[FROM]]
+  ICHUNK = ICHUNK + 1
+  CUR = CUR + LIMIT
+  
+  if(progress) pb = txtProgressBar(min = 0, max = NCHUNKS, initial = ICHUNK, style = 3) 
+  
+  while(ICHUNK <= NCHUNKS){
+    if(verbose){
+      cat(sprintf("%i:%i", ICHUNK, CUR))
+    }
+    if(progress) setTxtProgressBar(pb, ICHUNK)
+    x <- spocc::occ(query = species[1], limit = LIMIT, 
+                    from = FROM, start = CUR, 
+                    throw_warnings = FALSE)
+    xx[[ICHUNK]] <- x[[FROM]]
+    ICHUNK = ICHUNK + 1
+    CUR = CUR + LIMIT
+  }
+  
+  if(progress) close(pb)
+  
+  occurrence = lapply(xx, 
+    function(y){
+     return(y$data[[1]] |> 
+              dplyr::as_tibble() |> 
+              dplyr::select(-dplyr::any_of("networkKeys")))
+    }) |>
+    dplyr::bind_rows()
+  
+  
   if(cache == TRUE){
-    occurrence = readr::write_csv(occurrence, 
-                                  file.path(rappdirs::user_data_dir("gbif_niche"),
-                                            paste0(species[1], ".csv.gz")))
+    path <- get_path(paste0(species[1], ".csv.gz"))
+    occurrence <- readr::write_csv(occurrence, path)
   }
   return(occurrence)
 }
@@ -31,8 +100,7 @@ fetch_gbif = function(species = "Carcharodon carcharias", cache = TRUE){
 #' @param refresh logical, if true fetch fresh set of data
 #' @return data frame in the form of a tibble
 read_gbif = function(species = "Carcharodon carcharias", refresh = FALSE){
-  filename = file.path(rappdirs::user_data_dir("gbif_niche"),
-                       paste0(species[1], ".csv.gz"))
+  filename = get_path(paste0(species[1], ".csv.gz"))
   if (!file.exists(filename) || refresh == TRUE) {
     x = fetch_gbif(species = species)
   }
